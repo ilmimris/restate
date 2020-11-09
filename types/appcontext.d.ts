0da74d039f3bc641e432e03338b2a04ad2673bc8
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2020-11-09 15:40:06 +0700

    add initial support for typescript

---
+++ types/appcontext.d.ts
@@ -0,0 +1,4 @@
+export function ContextProviderHook(context: any, actionHandlers: any, initState: any): (props: any) => any;
+export function ContextConnector(context: any, stateFilter: any, dispatchPropsCreator: any, mixPropsCreator: any, dispatchPropName: any, methodsPropName: any): (Component: any) => (props: any) => any;
+export function renderActionObject(context: any, actionInstance: any, statePropMaps: any): any;
+export function yieldEventLoop(): Promise<any>;
